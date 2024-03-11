d6a0050d530cf60804e5b384e626d9345e65aba7
andryusha2002 <andrey.fedorenko2002@gmail.com>
2024-03-11 20:19:40 +0200

    first commit

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MainPage(User user)
+        {
+            if (user.Age >= 16)
+            {
+                TempData["Quantity"] = user.Age;
+                return RedirectToAction("OrderPage");
+            }
+            else
+            {
+                return Content("Вибачте, ви повинні бути старші за 16 років для замовлення товарів.");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult OrderPage()
+        {
+            if (TempData["Quantity"] == null)
+            {
+                return RedirectToAction("MainPage");
+            }
+            ViewBag.Quantity = TempData["Quantity"];
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConfirmOrder(Product product)
+        {
+            return View("OrderConfirmation", product);
+        }
+    }
+}
+
+
+
+
+
---
+++ WebApplication1/WebApplication1/Startup.cs
@@ -0,0 +1,59 @@
+﻿
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=MainPage}/{id?}"); 
+            });
+
+       
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path == "/")
+                {
+                    context.Response.Redirect("/Home/MainPage"); 
+                }
+                else
+                {
+                    await next();
+                }
+            });
+        }
+    }
+}
+
